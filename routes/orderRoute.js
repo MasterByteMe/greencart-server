a4ad6c1ff841f0aba174bf0098974066a4bc61e1
Mark Anthony Ruiz <mark.devz.08@gmail.com>
2025-10-31 22:16:40 +0800

    add post route for placeOrderStripe with user authentication

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import { getUserOrders, placedOrderCOD, getAllOrders } from '../controllers/orderController.js';
+import { getUserOrders, placedOrderCOD, getAllOrders, placedOrderStripe } from '../controllers/orderController.js';
 import authUser from "../middlewares/authUser.js"
-
+import authSeller from "../middlewares/authSeller.js";
 
 const orderRouter = express.Router();
 
 
-orderRouter.post('/cod', authUser, placedOrderCOD);
-orderRouter.post('/user', authUser, getUserOrders);
-orderRouter.post('/seller', authUser, getAllOrders);
+orderRouter.post('/cod', authUser, placedOrderCOD);  // 👈 Only logged-in users can place orders
+orderRouter.get('/user', authUser, getUserOrders);   // 👈 Only the logged-in user can view *their own* orders
+orderRouter.get('/seller', authSeller, getAllOrders); // 👈 Only sellers/admins can view *all* orders
+orderRouter.post('/stripe', authUser, placedOrderStripe);
 
 
 export default orderRouter;
