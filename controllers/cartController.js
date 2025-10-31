360e12e07a48aa2d92222fc65f3cdd45295815a1
Mark Anthony Ruiz <mark.devz.08@gmail.com>
2025-10-31 22:34:19 +0800

    refactor: extract userId from req object in updateCart
    * Updated  controller to use  instead of retrieving from client body
    * Improved security by preventing manual userId input from client
    * Simplified request handling and ensured consistent user authentication flow

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,23 +3,4 @@ import User from "../models/User.js";
 
 // Update User CartData: api/cart/update
 
-export const updateCart = async (req, res) => {
-    try {
-        // Extract userId and cartItems from the request body sent by the client
-        const userId = req.userId;
-        const { cartItems } = req.body;
-
-        // Find the user by ID and update their cart data in the database
-        await User.findByIdAndUpdate(userId, { cartItems });
-
-        // Send a success response if the cart update is successful
-        res.json({ success: true, message: "Cart Updated" });
-    } catch (error) {
-        // Log the error message for debugging
-        console.log(error.message);
-
-        // Send an error response if something goes wrong
-        res.json({ success: false, message: error.message });
-    }
-}
 
